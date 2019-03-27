3cbfea360ed1783d45fc926b284a840cf6250c82
krakitols <44405498+krakitols@users.noreply.github.com>
2019-03-27 21:14:48 +0300

    Add files via upload

---
+++ login_up.php
@@ -0,0 +1,40 @@
+
+<?php 
+
+session_start();
+
+$password = md5($_POST['password']);
+$email = $_POST['email'];
+
+ echo 'Пароль ' . $password . "</br>";
+echo 'логин ' . $email . "</br>";
+
+
+foreach ($_POST as $input) {
+	if (empty($input)) {
+ 	$errorMessage = "Заполните все поля";
+	include 'errors.php';
+	exit();	
+	}
+
+}
+$dsn = new PDO('mysql:host=localhost;dbname=test', 'root', '');	
+$connSqlCheck = 'SELECT id FROM	users WHERE password = :password or email = :email';
+$stmt = $dsn->prepare($connSqlCheck);
+$stmt->bindParam(':password', $password );
+$stmt->bindParam(':email', $email);
+$stmt->execute();
+$checkUser = $stmt->fetchColumn();
+var_dump($checkUser);
+
+if ($checkUser) {
+	header('Location: list.php');
+	exit();
+
+} else {
+	$errorMessage = "Email или пароль неправильный";
+    include 'errors.php';
+}
+
+
+ ?>
